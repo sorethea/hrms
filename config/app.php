f86a5ce757dabf3b25458d992710f568af5b6a7b
Sorethea OK <sorethea@gmail.com>
2024-05-03 09:12:43 +0700

    update

--- config/app.php
+++ config/app.php
@@ -170,7 +170,7 @@ return [
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        \Sorethea\Core\CoreServiceProvider::class,
+        //\Sorethea\Core\CoreServiceProvider::class,
     ])->toArray(),
 
     /*
