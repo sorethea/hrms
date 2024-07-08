855210a248aa6abdf25b34ae3d439f2769c3b469
Sorethea OK <sorethea@gmail.com>
2024-07-08 15:43:30 +0700

    Restaurant Module

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,6 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->navigation(false)
             ->spa()
             ->default()
             ->id('admin')
