e0649840c1d3bc164cd2d315d476e598e5e5a699
Sorethea OK <sorethea@gmail.com>
2024-08-09 11:46:10 +0700

    update EV package

--- packages/theA/ev/src/EvServiceProvider.php
+++ packages/theA/ev/src/EvServiceProvider.php
@@ -12,24 +12,10 @@ class EvServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name)
-            //->hasConfigFile()
+            ->hasConfigFile('vehicle')
             ->hasMigrations()
             ->hasViews('vehicle')
             ->hasTranslations();
     }
 
-    public function register(): void
-    {
-//        if (! app()->configurationIsCached()) {
-//            $this->mergeConfigFrom(__DIR__.'/../config/restaurant.php', 'restaurant');
-//        }
-
-//        $this->loadTranslationsFrom(__DIR__.'/../lang','restaurant');
-//        $this->loadViewsFrom(__DIR__.'/../resources/views','ev');
-    }
-
-    public function boot(): void
-    {
-        //$this->loadViewsFrom(__DIR__.'/../resources/views','ev');
-    }
 }
