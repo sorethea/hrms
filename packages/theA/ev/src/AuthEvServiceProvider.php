f7ee005749eb79809a852f6d6b1a29e2ac31a3c4
Sorethea OK <sorethea@gmail.com>
2024-08-09 09:54:43 +0700

    update VehiclePolicy

--- packages/theA/ev/src/AuthEvServiceProvider.php
+++ packages/theA/ev/src/AuthEvServiceProvider.php
@@ -3,9 +3,14 @@
 namespace Sorethea\Ev;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Sorethea\Ev\Models\Vehicle;
+use Sorethea\Ev\Policies\VehiclePolicy;
 
 class AuthEvServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        Vehicle::class => VehiclePolicy::class,
+    ];
     public function register(): void
     {
 
