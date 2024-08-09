d2792a3b6d5d1e2082f18ad8e810425fba92091d
Sorethea OK <sorethea@gmail.com>
2024-08-09 15:15:52 +0700

    update EV package

--- packages/theA/ev/src/EvServiceProvider.php
+++ packages/theA/ev/src/EvServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Sorethea\Ev;
 
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -13,9 +14,19 @@ class EvServiceProvider extends PackageServiceProvider
     {
         $package->name(static::$name)
             ->hasConfigFile('vehicle')
-            ->hasMigrations()
+            ->hasMigrations(['2024_08_09_020808_create_vehicles_table'])
             ->hasViews('vehicle')
-            ->hasTranslations();
+            ->hasTranslations()
+            ->hasInstallCommand(function (InstallCommand $command){
+                $command->startWith(function (InstallCommand $command){
+                    $command->info("Electric Vehicle Installation.");
+                })
+                ->publishConfigFile()
+                ->publishMigrations()
+                ->endWith(function (InstallCommand $command){
+                    $command->info("Installation Completed!");
+                });
+            });
     }
 
 }
