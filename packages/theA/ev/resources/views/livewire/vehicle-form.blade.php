fabe3ba5fa1e65c8f2a6e76338e27f9f73ba8e22
Sorethea OK <sorethea@gmail.com>
2024-08-12 14:44:13 +0700

    update EV package

--- packages/theA/ev/resources/views/livewire/vehicle-form.blade.php
+++ packages/theA/ev/resources/views/livewire/vehicle-form.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+<x-filament::card>
+    <form wire:submit.prevent="submit" class="space-y-6">
+        {{$this->form}}
+        <div class="text-right">
+            <x-filament::button type="submit" form="submit" class="align-right">
+                {{ __('filament-breezy::default.profile.password.submit.label') }}
+            </x-filament::button>
+        </div>
+    </form>
+</x-filament::card>
--- packages/theA/ev/src/Livewire/VehicleForm.php
+++ packages/theA/ev/src/Livewire/VehicleForm.php
@@ -2,10 +2,19 @@
 
 namespace Sorethea\Ev\Livewire;
 
+use Filament\Forms\Form;
 use Livewire\Component;
 
 class VehicleForm extends Component
 {
+    public function form(Form $form): Form {
+        return $form->schema([
+
+        ]);
+    }
+    public function submit(): void {
+
+    }
     public function render()
     {
         return view('vehicle::livewire.vehicle-form');
