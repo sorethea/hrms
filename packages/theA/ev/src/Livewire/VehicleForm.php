b27d666fd9eb4431e024e888d5bc3acfa94c3c53
Sorethea OK <sorethea@gmail.com>
2024-08-12 15:29:25 +0700

    update EV package

--- packages/theA/ev/src/Livewire/VehicleForm.php
+++ packages/theA/ev/src/Livewire/VehicleForm.php
@@ -5,6 +5,7 @@ namespace Sorethea\Ev\Livewire;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
@@ -21,9 +22,12 @@ class VehicleForm extends Component implements HasActions, HasForms
 
     public function form(Form $form): Form {
         return $form->schema([
-            TextInput::make('make')
-                ->label(__('ev::default.vehicle.make'))
-                ->required(),
+            Section::make([
+                TextInput::make('make')
+                    ->label(__('ev::default.vehicle.make'))
+                    ->required(),
+            ])->columns(2),
+
         ])->statePath('data');
     }
     public function submit(): void {
